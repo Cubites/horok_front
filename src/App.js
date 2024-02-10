9eb368d553ee22e4b87273edfe7a82c2dfabcb19
Cubites <75084369+Cubites@users.noreply.github.com>
2024-02-10 16:18:29 +0900

    Design: 하단 메뉴 디자인

--- src/App.js
+++ src/App.js
@@ -4,29 +4,17 @@ import { useEffect } from 'react';
 import HomeComponent from './components/home/HomeComponent';
 import PayComponent from './components/pay/PayComponent';
 import FolderAddComponenet from './components/folder/FolderAddComponent';
-import FolderListComponent from './components/folder/FolderAddComponent';
+import FolderListComponent from './components/folder/FolderListComponent';
 import MapComponent from './components/map/MapComponent';
 import MypageComponent from './components/mypage/MypageComponent';
 
 function App() {
   const showCenterBtns = (e) => {
     e.preventDefault();
-    let btnDisplay = document.getElementById("footerCenterBtnBox").style.display;
-    if(btnDisplay === "" || btnDisplay === "none") {
-      document.getElementById("footerCenterBtnBox").style.display = "flex";
-    } else {
-      document.getElementById("footerCenterBtnBox").style.display = "none";
-    }
+    document.getElementById("footerCenterBtnBox").classList.toggle('active');
   }
   useEffect(() => {
-    document.getElementById("footerCenterTitle").addEventListener("focusout", () => {
-      document.getElementById("footerCenterBtnBox").style.display = "none";
-    });
-    document.querySelectorAll("#footerCenterBtnBox>.folderBtn").forEach((tag) => {
-      tag.addEventListener("click", () => {
-        document.getElementById("footerCenterBtnBox").style.display = "none";
-      });
-    });
+
   }, []);
   return (
     <div id="container">
@@ -41,17 +29,45 @@ function App() {
         </Routes>
       </div>
       <div id="footer">
-        <div><Link to={"/"}>홈</Link></div>
-        <div><Link to={"/paylist"}>결제내역</Link></div>
+        <div>
+          <Link to={"/"}>
+            <img src='/images/home_icon.png' alt='home_icon' />
+            <span>홈</span>
+          </Link>
+        </div>
+        <div>
+          <Link to={"/paylist"}>
+            <img src='/images/receipt_icon.png' alt='receipt_icon' />
+            <span>결제내역</span>
+          </Link>
+        </div>
         <div id="footerCenter">
-          <div id="footerCenterTitle" onClick={showCenterBtns}>폴더</div>
+          <div id="footerCenterTitle" onClick={showCenterBtns}>
+            <img src='/images/folder_icon.png' alt='folder_icon' />
+          </div>
           <div id="footerCenterBtnBox">
-            <Link to={"/folder/add"} className='folderBtn'>폴더 추가</Link>
-            <Link to={"/folder/list"} className='folderBtn'>폴더 목록</Link>
+            <Link to={"/folder/add"} className='folderBtn'>
+              <img src='/images/folder_add_icon.png' alt='folder_add_icon' />
+              <span>폴더 추가</span>
+            </Link>
+            <Link to={"/folder/list"} className='folderBtn'>
+              <img src='/images/folder_list_icon.png' alt='folder_list_icon' />
+              <span>폴더 목록</span>
+            </Link>
           </div>
         </div>
-        <div><Link to={"/map"}>지도</Link></div>
-        <div><Link to={"/mypage"}>내 정보</Link></div>
+        <div>
+          <Link to={"/map"}>
+            <img src='/images/map_icon.png' alt='map_icon' />
+            <span>지도</span>
+          </Link>
+        </div>
+        <div>
+          <Link to={"/mypage"}>
+            <img src='/images/personal_menu_icon.png' alt='personal_menu_icon' />
+            <span>내 정보</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
